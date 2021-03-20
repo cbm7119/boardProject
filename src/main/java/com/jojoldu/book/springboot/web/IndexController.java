590ccaef24d30b78dc73ef8b01ba397e5b858d73
cbm7119 <komabomin9@gmail.com>
2021-03-20 15:10:58 +0900

    mustache - 화면구성1

---
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -0,0 +1,20 @@
+package com.jojoldu.book.springboot.web;
+
+import jdk.nashorn.internal.objects.annotations.Getter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+}
